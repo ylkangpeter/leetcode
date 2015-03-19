74d1a1ec1f9c8b8c543fa97697a6b85bfd90ac4b
peter <ylkangpeter@gmail.com>
2015-03-19 10:54:56 +0800

    more

--- Algorithms/src/peter/_189_Rotate_Array.java
+++ Algorithms/src/peter/_189_Rotate_Array.java
@@ -1,5 +1,7 @@
 package peter;
 
+import java.util.Arrays;
+
 /**
  * Rotate an array of n elements to the right by k steps.
  * 
@@ -18,7 +20,52 @@ package peter;
  */
 public class _189_Rotate_Array {
 
-	public void rotate_1(int[] nums, int k) {
-		for(int i=)
+	public static void rotate_1(int[] nums, int k) {
+		if (k % nums.length == 0) {
+			return;
+		}
+
+		k = k % nums.length;
+
+		reverse(nums, 0, nums.length - 1);
+		reverse(nums, 0, k - 1);
+		reverse(nums, k, nums.length - 1);
+
+	}
+
+	private static void reverse(int[] nums, int start, int end) {
+		for (int i = start; i <= (start + end) / 2; i++) {
+			int tmp = nums[i];
+			nums[i] = nums[end - i + start];
+			nums[end - i + start] = tmp;
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+		rotate_1(nums, 1);
+		System.out.println(Arrays.toString(nums));
+		nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		rotate_1(nums, 2);
+		System.out.println(Arrays.toString(nums));
+		nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		rotate_1(nums, 3);
+		System.out.println(Arrays.toString(nums));
+		nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		rotate_1(nums, 4);
+		System.out.println(Arrays.toString(nums));
+		nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		rotate_1(nums, 5);
+		System.out.println(Arrays.toString(nums));
+		nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		rotate_1(nums, 6);
+		System.out.println(Arrays.toString(nums));
+		nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		rotate_1(nums, 7);
+		System.out.println(Arrays.toString(nums));
+		nums = new int[] { 1 };
+		rotate_1(nums, 1);
+		System.out.println(Arrays.toString(nums));
+
 	}
 }
