c897716ef3ad9badc7eaf53244a7d4ee2b6b433a
peter <ylkangpeter@gmail.com>
2015-03-26 16:09:42 +0800

    more

--- Algorithms/src/peter/_004_Median_of_Two_Sorted_Arrays.java
+++ Algorithms/src/peter/_004_Median_of_Two_Sorted_Arrays.java
@@ -10,44 +10,41 @@ package peter;
 public class _004_Median_of_Two_Sorted_Arrays {
 
 	public static double findMedianSortedArrays(int A[], int B[]) {
-		int startA = 0;
-		int endA = A.length - 1;
-
-		int startB = 0;
-		int endB = B.length - 1;
-
-		int targetInx = (endB + endA) >>> 1;
-		int offset = 0;
-
-		while (offset < targetInx) {
-			int midA = (startA + endA) >>> 1;
-			int midB = (startB + endB) >>> 1;
-
-			if (A[midA] == B[midB]) {
-				while (offset < targetInx) {
-					if (A[midA] >= B[midB]) {
-						midA++;
-					} else {
-						midB++;
-					}
-					offset++;
-				}
-			} else if (A[midA] < B[midB]) {
-				offset = midA + startB;
-				startA = midA + 1;
-				endB = midB - 1;
-			} else {
-				offset = midB + startA;
-				startB = midB + 1;
-				endA = midA - 1 > startA ? midA - 1 : startA;
-			}
-
+		int m = A.length + B.length;
+		if (m % 2 == 0) {
+			int mid = findKth(A, B, 0, 0, m / 2)
+					+ findKth(A, B, 0, 0, m / 2 + 1);
+			return mid / 2.0;
+		} else {
+			return findKth(A, B, 0, 0, (m + 1) / 2);
 		}
+	}
 
-		if (startA == endA) {
+	private static int findKth(int[] A, int[] B, int aOffset, int bOffset, int k) {
+		if (A.length == aOffset)
+			return B[bOffset + k - 1];
+		if (B.length == bOffset)
+			return A[aOffset + k - 1];
+		if (k == 1)
+			return Math.min(A[aOffset], B[bOffset]);
 
+		int cut = min(A.length - aOffset - 1, B.length - bOffset - 1, k / 2 - 1);
+		if (A[aOffset + cut] < B[bOffset + cut]) {
+			aOffset = aOffset + cut + 1;
+		} else {
+			bOffset = bOffset + cut + 1;
 		}
-		return 0;
+		k = k - cut - 1;
+
+		return findKth(A, B, aOffset, bOffset, k);
+	}
+
+	private static int min(int a, int b, int c) {
+		if (a < b) {
+			return a < c ? a : c;
+		} else {
+			return b < c ? b : c;
+		}
 	}
 
 	public static void main(String[] args) {
