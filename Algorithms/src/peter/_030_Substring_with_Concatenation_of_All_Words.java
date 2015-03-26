868ef7ed2ac897a4c1f2d6e4c715c9d83e57e725
ylkangeter@gmail.com <ylkangpeter@gmail.com>
2015-03-26 22:27:30 +0800

    more

---
+++ Algorithms/src/peter/_030_Substring_with_Concatenation_of_All_Words.java
@@ -0,0 +1,23 @@
+package peter;
+
+import java.util.List;
+
+/**
+ * You are given a string, S, and a list of words, L, that are all of the same
+ * length. Find all starting indices of substring(s) in S that is a
+ * concatenation of each word in L exactly once and without any intervening
+ * characters.
+ * 
+ * For example, given: S: "barfoothefoobarman" L: ["foo", "bar"]
+ * 
+ * You should return the indices: [0,9]. (order does not matter).
+ * 
+ * @author peter Mar 26, 2015
+ * 
+ */
+public class _030_Substring_with_Concatenation_of_All_Words {
+
+    public static List<Integer> findSubstring(String S, String[] L) {
+        
+    }
+}
