ebae9542f867b36625f9578e2931300d44d39a04
peter <ylkangpeter@gmail.com>
2015-05-04 13:35:57 +0800

    add auto check new problems script

---
+++ tool/leetcode.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+mail=$1
+
+rm -fr *.tmp
+rm -fr *.res
+#rm shell
+
+url1="https://leetcode.com/problemset/algorithms/"
+url2="https://leetcode.com/problemset/database/"
+url3="https://leetcode.com/problemset/shell/"
+
+function check () {
+  curl $1 | grep 'span class=\"None\"' >$2
+
+  if test -s $2;
+  then
+    wc -l $2 > ${2}.tmp
+    diff ${2}.tmp ${2}.cur > ${2}.res
+    if test -s ${2}.res;
+    then
+      echo "update in "${2} |mail -s "leetcode problem updated!" $3
+    fi
+  fi
+}
+
+check $url1 alogrithms $mail
+check $url2 database $mail
+check $url3 shell $mail
+
